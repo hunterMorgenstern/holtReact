0c97d45ce84ed30d000dcdc7d39a338823cbcbec
Hunter Morgenstern <hunter.morgenstern@gmail.com>
2018-08-07 13:04:11 -0700

    added stateful search input

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import preload from "../data.json";
 import ShowCard from "./ShowCard";
 
-const Search = () => (
-  <div className="search">
-    <div>
-      {preload.shows.map(show => (
-        <ShowCard {...show} show={show} key={show.imdbID} />
-      ))}
-    </div>
-  </div>
-);
+class Search extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchTerm: "this is some sort"
+    };
+  }
+  render() {
+    return (
+      <div className="search">
+        <header>
+          <h1>svideo</h1>
+          <input value={this.state.searchTerm} type="text" placeholder="Search" />
+        </header>
+        <div>
+          {preload.shows.map(show => (
+            <ShowCard {...show} show={show} key={show.imdbID} />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Search;
