c0aea3f28b697f9f24cec9f12b4b6b109959200f
Hunter Morgenstern <hunter.morgenstern@gmail.com>
2018-08-08 18:30:07 -0700

    flow initialized on project

--- __test__/Search.spec.jsx
+++ __test__/Search.spec.jsx
@@ -5,18 +5,18 @@ import Search from "../js/Search";
 import ShowCard from "../js/ShowCard";
 
 test("Search renders correcly", () => {
-  const component = shallow(<Search />);
+  const component = shallow(<Search shows={preload.shows} />);
   expect(component).toMatchSnapshot();
 });
 
 test("Search should render correct amount of shows", () => {
-  const component = shallow(<Search />);
+  const component = shallow(<Search shows={preload.shows} />);
   expect(component.find(ShowCard).length).toEqual(preload.shows.length);
 });
 
 test("Search should render correct amount of shows based on search term", () => {
   const searchWord = "black";
-  const component = shallow(<Search />);
+  const component = shallow(<Search shows={preload.shows} />);
   component.find("input").simulate("change", { target: { value: searchWord } });
   const showCount = preload.shows.filter(
     show => `${show.title} ${show.description}`.toUpperCase().indexOf(searchWord.toUpperCase()) >= 0
